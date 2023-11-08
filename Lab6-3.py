eca4768b6105c57318a6231cb027429766ed53fa
P24aelsheikh <144148097+P24aelsheikh@users.noreply.github.com>
2023-11-08 13:35:53 -0500

    Add files via upload

---
+++ Lab6-3.py
@@ -0,0 +1,23 @@
+"""
+Create a Python file named lab_6-3
+
+*** You must write a comment for every chunk of code you write from now on along with your author tag***
+
+Create a list of 4 colors and store it as a variable.
+Use a method to add 3 more colors to the list in a single statement.
+Use a different method to add one additional color to the list.
+Use a method to insert a new color at index 3.
+Use a method to create a copy of the list
+Use a method to remove one element from the copy of the list.
+
+"""
+#Alsir ELsheikh 11/8/2023
+
+colors = ["red", "blue", "green", "yellow"]
+colors.extend(["orange", "purple", "pink"])
+colors.append("brown")
+colors.insert(3, "violet")
+colors_copy = colors.copy()
+colors_copy.pop(2)
+print("Original List:", colors)
+print("Modified Copy:", colors_copy)
